940cbec2e3f79fdf234ddcee772e6c61b33dea6c
Elias <elias@localhost.localdomain>
2026-04-30 15:10:15 -0600

    Cambios de nosotros.pug y creado inicio.pug

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,16 @@ import express, { Router } from "express";
 const router = express.Router();
 
 router.get('/', (req, res) => { // req - lo que enviamos : res - lo que recibimos
-    res.send('Inicio')
+    res.render('inicio')
 });
 
 router.get('/nosotros', (req, res) => { // req - lo que enviamos : res - lo que recibimos
-    res.render('nosotros')
+   
+   const viajes = 'Viaje a Japón';
+   
+    res.render('nosotros', {
+        viajes
+    })
 });
 
 router.get('/contacto', (req, res) => { // req - lo que enviamos : res - lo que recibimos
