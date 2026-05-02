86526f66e144daf10f31eb58a077a6b3891292a6
Elias-J-Guardado <eliasjguardado2001@gmail.com>
2026-05-01 18:16:51 -0600

    Añadiendo hojas de estilos

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const port = process.env.PORT || 4000;
 // Habilitando pug
 app.set('view engine', 'pug');
 
+// Definir la carpeta publica
+app.use(express.static('public'));
+
 //Agregar router
 app.use('/', router);
 
