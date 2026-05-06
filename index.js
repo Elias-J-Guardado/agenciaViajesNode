23337c6fe91416eceb628d00f0dcee3114d8961a
Elias-J-Guardado <eliasjguardado2001@gmail.com>
2026-05-06 16:42:02 -0600

    Add Footer and dinamic year variable

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ const port = process.env.PORT || 4000;
 // Habilitando pug
 app.set('view engine', 'pug');
 
+// Obtener año actual
+app.use( (req, res, next) => {
+    const year = new Date();
+    res.locals.actualYear = year.getFullYear();
+
+    next();
+})
+
 // Definir la carpeta publica
 app.use(express.static('public'));
 
