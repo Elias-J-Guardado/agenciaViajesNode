4ccb427b63aeb5ea32105e702594142cacf9a140
Elias-J-Guardado <eliasjguardado2001@gmail.com>
2026-05-07 15:17:01 -0600

    Añadiendorutas y archivos .pug

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,27 @@ import express, { Router } from "express";
 const router = express.Router();
 
 router.get('/', (req, res) => { // req - lo que enviamos : res - lo que recibimos
-    res.render('inicio')
-});
-
-router.get('/nosotros', (req, res) => { // req - lo que enviamos : res - lo que recibimos
-   
-   const viajes = 'Viaje a Japón';
-   
-    res.render('nosotros', {
-        viajes
+    res.render('inicio', {
+        pagina: 'Inicio'
     })
 });
 
-router.get('/contacto', (req, res) => { // req - lo que enviamos : res - lo que recibimos
-    res.send('Contacto')
+router.get('/nosotros', (req, res) => { // req - lo que enviamos : res - lo que recibimos
+    res.render('nosotros', {
+        pagina: 'Nosotros'
+    })
+});
+
+router.get('/viajes', (req, res) => { // req - lo que enviamos : res - lo que recibimos
+    res.render('viajes', {
+        pagina: 'Viajes'
+    })
+});
+
+router.get('/testimoniales', (req, res) => { // req - lo que enviamos : res - lo que recibimos
+    res.render('testimoniales', {
+        pagina: 'Testimoniales'
+    })
 });
 
 
