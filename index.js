a886d7800f264d17ea3be58e1fb0799c3d16d1ac
Elias-J-Guardado <eliasjguardado2001@gmail.com>
2026-05-11 10:56:01 -0600

    Add database conecction

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // const express = require('express'); common js
 import express from "express";
 import router from './routes/index.js';
+import db from './config/db.js';
 
 const app = express();
+
+//conectar la db
+db.authenticate()
+    .then(() => console.log('Base de datos iniciada'))
+    .catch(error => console.log(error));
 
 //definir puerto
 const port = process.env.PORT || 4000;
