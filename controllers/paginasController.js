28c36f6b0798c807ae66efb57bbf4dca6569c0fb
Elias-J-Guardado <eliasjguardado2001@gmail.com>
2026-05-12 15:42:34 -0600

    feat: add paginasController and refactor router to use it

---
+++ controllers/paginasController.js
@@ -0,0 +1,30 @@
+const paginaInicio = (req, res) => { // req - lo que enviamos : res - lo que recibimos
+    res.render('inicio', {
+        pagina: 'Inicio'
+    })
+} 
+
+const paginaNosotros = (req, res) => { // req - lo que enviamos : res - lo que recibimos
+    res.render('nosotros', {
+        pagina: 'Nosotros'
+    })
+}
+
+const paginaViajes =  (req, res) => { // req - lo que enviamos : res - lo que recibimos
+    res.render('viajes', {
+        pagina: 'Viajes'
+    })
+}
+
+const paginaTestimoniales = (req, res) => { // req - lo que enviamos : res - lo que recibimos
+    res.render('testimoniales', {
+        pagina: 'Testimoniales'
+    })
+}
+
+export{
+    paginaInicio,
+    paginaNosotros,
+    paginaViajes,
+    paginaTestimoniales
+}
