b6c71441b9be7741edaa13a324165f170d028398
Elias Guardado <eliasjguardado2001@gmail.com>
2026-05-25 15:25:07 -0600

    Add environment variables changes

--- config/db.js
+++ config/db.js
@@ -2,9 +2,7 @@ import Sequelize from "sequelize";
 import dotenv from "dotenv";
 dotenv.config()
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    port: '3306',
+const db = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'mysql',
     define: {
         timestamps: false
@@ -15,7 +13,8 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.D
         acquire: 30000,
         idle: 10000
     },
-    OperatorAliases: false
+    OperatorAliases: false,
+    logging: console.log
 });
 
 
